b148c56ede30bbbaa01392c2266e15fa862ee8f1
Nicolas Linden <57541749+nplinden@users.noreply.github.com>
2024-06-05 09:18:13 +0200

    Explicitely using openpyxl as default engine for writing to excel (#317)
    * engine is openpyxl
    
    * engine is openpyxl
    
    ---------
    
    Co-authored-by: Nicolas Linden <n.linden@naarea.fr>

--- sandy/core/cov.py
+++ sandy/core/cov.py
@@ -622,7 +622,7 @@ class CategoryCov():
 
         # -- Save seed and singulare values
         if to_excel:
-            with pd.ExcelWriter(to_excel, mode='w') as writer:
+            with pd.ExcelWriter(to_excel, mode='w', engine='openpyxl') as writer:
                 pd.Series(seed_).to_excel(writer, header=None, index=None, sheet_name='SEED')
                 pd.Series(Sr).to_excel(writer, header=None, index=None, sheet_name='SVALUES')
 
@@ -647,7 +647,7 @@ class CategoryCov():
 
         # -- Save samples
         if to_excel:
-            with pd.ExcelWriter(to_excel, mode='a') as writer:
+            with pd.ExcelWriter(to_excel, mode='a', engine="openpyxl") as writer:
                 # this might go into Samples
                 s = samples.reset_index()
                 loc = s.columns.get_loc("E")
